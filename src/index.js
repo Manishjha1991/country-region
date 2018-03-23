8eff6588c924f092edcff2e8149e9f3726c73d8b
Manishjha1991 <mkjha9555@gmail.com>
2018-03-23 20:50:08 +0530

    add package and function in src

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+var ucfirst = require('ucfirst');
+module.exports = function (country) { 
+      let capsCountry=ucfirst(country);
+      var countriesJson = JSON.parse(fs.readFileSync("./src/country.json", 'utf8'));
+      return _.find(countriesJson, function(o) { return o.countryName ===country});
+}
