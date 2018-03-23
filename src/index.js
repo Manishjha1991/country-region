b613e45ee6ab975a0fdd7c71d911ac42613ef6a8
Manishjha1991 <mkjha9555@gmail.com>
2018-03-23 21:22:22 +0530

    add package and remve fs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 var _ = require('lodash');
 var ucfirst = require('ucfirst');
-var fs = require("fs");
 module.exports = function (country) { 
       let capsCountry=ucfirst(country);
       var countriesJson = JSON.parse(fs.readFileSync("./src/country.json", 'utf8'));
